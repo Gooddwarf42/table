bec006288ed82c955958f1efec4dc6436a66c16a
Marco <marcogirarditesero@gmail.com>
2023-03-21 22:05:35 +0000

    Setting up PrimeVue

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import PrimeVue from 'primevue/config';
+
 import './assets/main.css'
 
-createApp(App).mount('#app')
+
+//theme
+import "primevue/resources/themes/lara-light-indigo/theme.css";     
+//core
+import "primevue/resources/primevue.min.css";
+//icons
+import "primeicons/primeicons.css";
+
+const app = createApp(App);
+app.use(PrimeVue);
+app.mount('#app');
